d5c18cfaafdf9ada043ecb12923662d4f4a647b5
CesstialZT <103538828+CesstialZT@users.noreply.github.com>
2022-05-19 15:38:35 -0500

    Delete cesstial-currency-dev directory

--- cesstial-currency-dev/functions/interactions/buttons.js
+++
@@ -1,4 +0,0 @@
-// Since we're using the button.interaction.all trigger we split the files up for orginisation.
-// Here we just call those files.
-const custom_id = context.params.event.data.custom_id;
-await require(`../../helper/buttons/${custom_id}`)(context);
--- cesstial-currency-dev/helper/buttons/transaction_refuse.js
+++
@@ -1,9 +0,0 @@
-module.exports = async (context) => {
-  // Authenticates you with the API standard library
-  // Type `await lib.` to display API autocomplete
-  const lib = require('lib')({token: process.env.STDLIB_SECRET_TOKEN});
-  
-  await lib.discord.interactions['@1.0.1'].responses.destroy({
-    token: context.params.event.token,
-  });
-}
